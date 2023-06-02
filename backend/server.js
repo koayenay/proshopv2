0964cf33808ce97dec06da8f7f7fe9faa706e4d9
koayenay <koayenay@gmail.com>
2023-06-02 15:58:15 -0700

    setup env

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,26 @@
-import express from "express";
-import products from "./data/products.js";
-const port = 5000;
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+import products from "./data/products.js"
 
-const app = express();
+const port = process.env.PORT || 5000
+
+const app = express()
 
 // Route to handle the root URL
 app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+  res.send("API is running...")
+})
 
 // Route to get all products
 app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+  res.json(products)
+})
 
 // Route to get a specific product by ID
 app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`))
