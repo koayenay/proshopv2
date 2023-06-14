b9e3252071d2bd98143f1db6ba06d4c1303d3b21
koayenay <koayenay@gmail.com>
2023-06-13 22:56:25 -0700

    worked out Logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,6 +46,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/logout
 // accesss Private
 const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  })
+
+  res.status(200).json({ message: "Logged Out successfully" })
   res.send("logout user")
 })
 
