bc138d1b78d08e8b73d3c8d23472acd1887bad55
koayenay <koayenay@gmail.com>
2023-06-21 09:45:28 -0700

    started on placeorderscreen

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,9 +1,13 @@
-import React from "react"
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 import { Link, useNavigate } from "react-router-dom"
-import { useSelector } from "react-redux"
+import { toast } from "react-toastify"
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap"
-import CheckOutSteps from "../components/CheckOutSteps"
+import { useDispatch, useSelector } from "react-redux"
+import Message from "../components/Message"
+import CheckoutSteps from "../components/CheckoutSteps"
+import Loader from "../components/Loader"
+import { useCreateOrderMutation } from "../slices/ordersApiSlice"
+import { clearCartItems } from "../slices/cartSlice"
 
 const PlaceOrderScreen = () => {
   const navigate = useNavigate()
