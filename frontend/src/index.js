b7c3bd35fa1c7f77d9d6eae35f9cfa06a63dfc45
koayenay <koayenay@gmail.com>
2023-06-21 09:43:30 -0700

    order api slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import RegisterScreen from "./screens/RegisterScreen"
 import ShippingScreen from "./screens/ShippingScreen"
 import PrivateRoute from "./components/PrivateRoute"
 import PaymentScreen from "./screens/PaymentScreen"
+import PlaceOrderScreen from "./screens/PlaceOrderScreen"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -33,6 +34,7 @@ const router = createBrowserRouter(
       <Route path='' element={<PrivateRoute />}>
         <Route path='/shipping' element={<ShippingScreen />}></Route>
         <Route path='/payment' element={<PaymentScreen />}></Route>
+        <Route path='/placeorder' element={<PlaceOrderScreen />}></Route>
       </Route>
     </Route>
   )
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -40,6 +40,10 @@ const cartSlice = createSlice({
       state.paymentMethod = action.payload
       return updateCart(state)
     },
+    clearCartItems: (state, action) => {
+      state.cartItems = []
+      return updateCart(state)
+    },
   },
 })
 export const {
@@ -47,5 +51,6 @@ export const {
   removeFromCart,
   saveShippingAddress,
   savePaymentMethod,
+  clearCartItems,
 } = cartSlice.actions
 export default cartSlice.reducer
