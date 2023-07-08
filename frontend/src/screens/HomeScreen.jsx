9e695ddd12bd13a3718e4c631b0c4e9d64d95c3f
koayenay <koayenay@gmail.com>
2023-07-08 10:08:24 -0700

    paginate products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,6 +2,7 @@
 // import axios from "axios"
 import { Row, Col } from "react-bootstrap"
 import Product from "../components/Product"
+import { useParams } from "react-router-dom"
 import { useGetProductsQuery } from "../slices/productsApiSlice"
 import Loader from "../components/Loader"
 import Message from "../components/Message"
@@ -16,8 +17,8 @@ const HomeScreen = () => {
   //   }
   //   fetchProducts()
   // }, [])
-
-  const { data: products, isLoading, error } = useGetProductsQuery()
+  const { pageNumber } = useParams()
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber })
 
   return (
     <>
@@ -31,7 +32,7 @@ const HomeScreen = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product}></Product>
               </Col>
